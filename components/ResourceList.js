ae22f5c40bbeaef67e15bd9624b5335606be23de
eben-k <ennim.consults@gmail.com>
2019-11-23 23:59:59 +0100

    edit a product - build UI for editing a product - add functionality for editing a product

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -2,6 +2,8 @@ import gql from "graphql-tag";
 import { Query } from 'react-apollo';
 import { Card, ResourceList, Stack, TextStyle, Thumbnail } from '@shopify/polaris';
 import store from 'store-js';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Context } from '@shopify/app-bridge-react';
 
 const GET_PRODUCTS_BY_ID = gql`
 query getProducts($ids: [ID!]!) {
@@ -32,7 +34,19 @@ query getProducts($ids: [ID!]!) {
 }` ;
 
 class ResourceListWithProducts extends React.Component {
+
+    static contextType = Context;
+
     render() {
+        const app = this.context;
+        const redirectToProduct = () => {
+            const redirect = Redirect.create(app);
+            redirect.dispatch(
+                Redirect.Action.APP,
+                '/edit-products',
+            );
+        };
+
         const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
         return (
             <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
@@ -66,6 +80,10 @@ class ResourceListWithProducts extends React.Component {
                                       id={item.id}
                                       media={media}
                                       accessibilityLabel={`View details for ${item.title}`}
+                                      onClick={() => {
+                                          store.set('item', item);
+                                          redirectToProduct();
+                                      }}
                                       >
                                           <Stack>
                                               <Stack.Item fill>
